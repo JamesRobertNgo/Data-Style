9904e8058bf7e32b175b95e6b5bbccf12f8de788
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2016-08-20 19:19:35 -0400

    Update data_style.js

--- data_style.js
+++ data_style.js
@@ -1,18 +1,52 @@
-function dataStyle(attrib, media) {
-	var def = [];
+
+// Main function
+// E.g. dataStyles([['data-style-all', 'all'], ['data-style-tablet', 'all and (min-width: 500px)'], ['data-style-desktop', 'all and (min-width: 720px)']]);
+function dataStyles(mediaMap) {
 	
-	var elements = document.querySelectorAll('[' + attrib + ']');
+	// 1. Build selector from mediaMap (parameter)
+	var selector = '';
+	for (var i = 0; i < mediaMap.length; i++) {
+		if (selector.length > 0) 
+			selector += ',';
+		selector += '[' + mediaMap[i][0] + ']';
+	}
+	
+	// 2. Get all elements using the selector
+	var elements = document.querySelectorAll(selector);
+	
+	// 3. Build style definitions
+	var def = '';
 	for (var i = 0; i < elements.length; i++) {
 		var id = elements[i].getAttribute('id');
 		if (!id) {
-			id = 'data-style-' + Math.floor(Math.random() * 1000000);
+			id = 'id' + Math.floor(Math.random() * 1000000);
 			elements[i].setAttribute('id', id);
 		}
-		def.push('#' + id + ' { ' + elements[i].getAttribute(attrib) + ' } ');
+		
+		// 3.1 Add comment identifying the element
+		def += '/* -- #' + id + ' -- */';
+		
+		// 3.2 Add value from the style attribute
+		if (elements[i].getAttribute('style')) {
+			def += '#' + id + ' { ' + elements[i].getAttribute('style') + ' } ';
+			elements[i].removeAttribute('style'); // ?? Remove style attribute after ??
+		}
+		
+		// 3.3 Add values from attributes defined by the mediaMap
+		for (var j = 0; j < mediaMap.length; j++) {
+			var attr = mediaMap[j][0]; // Attribute
+			var style = elements[i].getAttribute(attr); // Attribute value
+			
+			// 3.3.1 Add attribute value (aka style) only when available
+			if (style) {
+				def += '@media ' + mediaMap[j][1] + ' { #' + id + ' { ' + style + ' } } ';
+				elements[i].removeAttribute(attr); // ?? Remove attribute after ??
+			}
+		}
 	}
 	
+	// 4. Create style tag and append to the head tag
 	var style = document.createElement('style');
-	style.setAttribute('media', media);
-	style.innerHTML = def.join('\n');
+	style.innerHTML = def.replace(/\s+/g, ' ').replace(/({|}|;|\*\/)\s*/g, '$1\n');
 	document.head.appendChild(style);
 }
